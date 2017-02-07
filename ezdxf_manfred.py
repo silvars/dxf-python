57ff437e0a4861f46c8da655661084835fb0357e
Rodrigo Matosm <rodrigo.matos@b2wdigital.com>
2017-02-07 10:28:07 -0200

    ok

--- ezdxf_manfred.py
+++ ezdxf_manfred.py
@@ -1,20 +1,14 @@
 import ezdxf
 
-dwg = ezdxf.new('R12')  # R12 for compatibility R2010 for more features
-
-dwg.styles.new('xxxx', dxfattribs={'font': 'MITSUBISHI 2015.ttf'})  # http://ezdxf.readthedocs.io/en/latest/tables.html#style
-
-# the advantage of dxfattribs={} are simple method signatures, and you can reuse property dicts
-
-# adding text:
+dwg = ezdxf.new('AC1009')  # TEXT is a basic entity and exists in every DXF standard
 
 msp = dwg.modelspace()
 
-#msp.add_text('* EU 872349 *', dxfattribs={ 'insert': (2.0, 2.0),  'height': 0.7})  # maybe with typos http://ezdxf.readthedocs.io/en/latest/entities.html#text
-msp.add_text('* EU 872349 *', dxfattribs={ 'style': 'xxxx', 'height': 0.7}).set_pos((2, 2), align='LEFT')
+# use set_pos() for proper TEXT alignment - the relations between halign, valign, insert and align_point are tricky.
+msp.add_text("A Simple Text").set_pos((2, 3), align='MIDDLE_RIGHT')
 
+# using text styles
+dwg.styles.new('custom', dxfattribs={'font': '/home/rodrigo/Downloads/MITSUBISHI 2015.ttf', 'width': 0.8})  # Arial, default width factor of 0.8
+msp.add_text("L", dxfattribs={'style': 'custom', 'height': 0.35}).set_pos((2, 6), align='LEFT')
 
-# .add_text() factory functions, work the same way in modelspace, layouts and block layouts.
-
-
-dwg.saveas('filename.dxf')
+dwg.saveas("mit.dxf")
--- ezdxf_manfred2.py
+++ ezdxf_manfred2.py
@@ -1,7 +1,14 @@
 import ezdxf
-dwg = ezdxf.new('R12')  
-dwg.styles.new('xxxx', dxfattribs={'font': 'MITSUBISHI 2015.ttf'}) 
-x = dwg.styles
+
+dwg = ezdxf.new('AC1009')  # TEXT is a basic entity and exists in every DXF standard
+
 msp = dwg.modelspace()
-msp.add_text('* EU 872349 *', dxfattribs={ 'style': 'xxxx', 'height': 0.7}).set_pos((2, 2), align='LEFT')
-dwg.saveas('customFile.dxf')
+
+# use set_pos() for proper TEXT alignment - the relations between halign, valign, insert and align_point are tricky.
+msp.add_text("A Simple Text").set_pos((2, 3), align='MIDDLE_RIGHT')
+
+# using text styles
+dwg.styles.new('custom', dxfattribs={'font': 'times.ttf', 'width': 0.8})  # Arial, default width factor of 0.8
+msp.add_text("Text Style Example: Times New Roman", dxfattribs={'style': 'custom', 'height': 0.35}).set_pos((2, 6), align='LEFT')
+
+dwg.saveas("simple_text.dxf")
